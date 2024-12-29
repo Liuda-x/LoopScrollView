ffc4e5efa40e8204ea9660c3b703c9e2049dc25c
Liuda-x <liudas8323@outlook.com>
2024-12-29 15:42:33 +0800

    上传示例代码

---
+++ SimpleLoopTest/SimpleLoopItemView.cs
@@ -0,0 +1,10 @@
+﻿using Liudax.LoopScrollView;
+
+public class SimpleLoopItemView : LoopItemView<string>
+{
+    public TMPro.TMP_Text text;
+    public override void ItemUpdate()
+    {
+        text.text = Data;
+    }
+}
---
+++ SimpleLoopTest/SimpleLoopScrollView.cs
@@ -0,0 +1,25 @@
+﻿using Liudax.LoopScrollView;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleLoopScrollView : MonoBehaviour
+{
+     public LoopScrollView loopView;
+    public int spawnCount = 5;
+    public int jump = 0;
+    public void Start()
+    {
+        loopView.Init();
+        List<string > list= new List<string>();
+        for(int i = 0; i < spawnCount; i++)
+        {
+            list.Add($"item:{i}");
+        }
+        loopView.SetData(list, jump);
+        //loopView.MoveToIndex(99);
+    }
+    public void OnDestroy()
+    {
+        loopView.Dispse();
+    }
+}
